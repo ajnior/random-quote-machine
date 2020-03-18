fc2700ff2a51d6379846b84d339ab734756c5e4e
Agnaldo Cardoso Junior <agnaldo.junior@grupoboticario.com.br>
2020-03-18 07:56:07 -0300

    adding font awesome

--- src/containers/QuoteBox/QuoteBox.js
+++ src/containers/QuoteBox/QuoteBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useFetch} from '../../hooks/useFetch';
+import FontAwesome from 'react-fontawesome';
 import { Wrapper } from './styles';
 
 function QuoteBox() {
@@ -8,12 +9,14 @@ function QuoteBox() {
   return (
     <Wrapper id="quote-box">
       <div className='text-container'>
-        <h1 id="text">{content}</h1>
+        <h1 id="text">{`"${content}"`}</h1>
         <h2 id="author">{`- ${author}`}</h2>
       </div>
       <div className='buttons-container'>
         <div className='buttons-container__social'>
-
+        <button>
+          <FontAwesome className="fab fa-twitter" size='2x' />
+        </button>
         </div>
         <div className='buttons-container__quote'>
           <button id="new-quote" onClick={fetchData}>New quote</button>
