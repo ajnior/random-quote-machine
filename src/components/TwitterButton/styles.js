32718199b00566b82128bd477ee17a7fcfca7920
Agnaldo Cardoso Junior <agnaldo.junior@grupoboticario.com.br>
2020-03-22 22:00:07 -0300

    fix: buttons styling not being applied by styled components

--- src/components/TwitterButton/styles.js
+++ src/components/TwitterButton/styles.js
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export const Anchor = styled.a`
   color: #00acee;
+  text-decoration: none;
 `;
--- src/containers/QuoteBox/styles.js
+++ src/containers/QuoteBox/styles.js
@@ -16,18 +16,43 @@ export const Box = styled.section`
   padding: 2.5rem;
   background: #ffffff;
   box-shadow: 0px 2px 5px -3px #64a3b1;
+
   .buttons-container {
     display: flex;
     justify-content: space-between;
     align-items: center;
     margin-top: 40px;
   }
+
   #text {
     font-weight: 300;
     letter-spacing: 0.02rem;
     line-height: 2.2rem;
   }
+
   #author {
     font-size: 1.2rem;
+  }
+
+  #tweet-quote {
+    color: #00acee;
+    text-decoration: none;
+  }
+
+  #new-quote {
+    background: #66aebd;
+    padding: 14px 41px;
+    color: #ffffff;
+    font-size: 0.8rem;
+    text-transform: uppercase;
+    font-family: sans-serif;
+    font-weight: 400;
+    border: none;
+    border-radius: 2px;
+    outline: none;
+    cursor: pointer;
+    &:hover {
+      background: #17839a;
+    }
   }
 `;
